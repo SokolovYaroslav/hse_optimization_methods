987fb0f5c6e0796c61fd955639dba2b4dbe34076
Yaroslav Sokolov <sokolov.yas@gmail.com>
2020-10-13 22:22:11 +0300

    Done the best solution. Made an analisys

--- oracul.py
+++ oracul.py
@@ -2,63 +2,51 @@ from math import sin, exp, pi, log, cos
 from typing import Callable, List, Tuple, Optional
 
 
-def f0(x):
-    return x ** 2 / 2, x
+f0 = lambda x: (x ** 2 / 2, x)
+
+f2 = lambda x: (sin(x) + sin(10 / 3 * x), cos(x) + 10 / 3 * cos(10 / 3 * x))
+
+f3 = lambda x: (
+    -sum(k * sin((k + 1) * x + k) for k in range(1, 6 + 1)),
+    -sum(k * (k + 1) * cos((k + 1) * x + k) for k in range(1, 6 + 1)),
+)
+
+f4 = lambda x: (
+    -(16 * x ** 2 - 24 * x + 5) * exp(-x),
+    (16 * x ** 2 - 56 * x + 29) * exp(-x),
+)
+
+f5 = lambda x: (
+    -(1.4 - 3 * x) * sin(18 * x),
+    3 * (18 * (x - 0.466667) * cos(18 * x) + sin(18 * x)),
+)
+
+f6 = lambda x: (
+    -(x + sin(x)) * exp(-(x ** 2)),
+    exp(-(x ** 2)) * (2 * x ** 2 + 2 * x * sin(x) - cos(x) - 1),
+)
+
+f7 = lambda x: (
+    sin(x) + sin(10 / 3 * x) + log(x) - 0.84 * x + 3,
+    1 / x + cos(x) + 10 / 3 * cos(10 / 3 * x) - 0.84,
+)
+
+f9 = lambda x: (sin(x) + sin(2 / 3 * x), cos(x) + 2 / 3 * cos(2 / 3 * x))
+
+f10 = lambda x: (-x * sin(x), -sin(x) - x * cos(x))
+
+f11 = lambda x: (2 * cos(x) + cos(2 * x), -2 * (sin(x) + sin(2 * x)))
+
+f12 = lambda x: (sin(x) ** 3 + cos(x) ** 3, 3 * sin(x) * cos(x) * (sin(x) - cos(x)))
+
+f18 = lambda x: (
+    ((x - 2) ** 2, 2 * (x - 2)) if x <= 3 else (2 * log(x - 2) + 1, 2 / (x - 2))
+)
 
 
-def f2(x):
-    return sin(x) + sin(10 / 3 * x), cos(x) + 10 / 3 * cos(10 / 3 * x)
-
-
-def f3(x):
-    val = 0
-    dval = 0
-    for k in range(1, 6 + 1):
-        val -= k * sin((k + 1) * x + k)
-        dval -= k * (k + 1) * cos((k + 1) * x + k)
-    return val, dval
-
-
-def f4(x):
-    return -(16 * x ** 2 - 24 * x + 5) * exp(-x), (16 * x ** 2 - 56 * x + 29) * exp(-x)
-
-
-def f5(x):
-    return -(1.4 - 3 * x) * sin(18 * x), 3 * (18 * (x - 0.466667) * cos(18 * x) + sin(18 * x))
-
-
-def f6(x):
-    return -(x + sin(x)) * exp(-x ** 2), exp(-x ** 2) * (2 * x ** 2 + 2 * x * sin(x) - cos(x) - 1)
-
-
-def f7(x):
-    return sin(x) + sin(10 / 3 * x) + log(x) - 0.84 * x + 3, 1 / x + cos(x) + 10 / 3 * cos(10 / 3 * x) - 0.84
-
-
-def f9(x):
-    return sin(x) + sin(2 / 3 * x), cos(x) + 2 / 3 * cos(2 / 3 * x)
-
-
-def f10(x):
-    return -x * sin(x), -sin(x) - x * cos(x)
-
-
-def f11(x):
-    return 2 * cos(x) + cos(2 * x), -2 * (sin(x) + sin(2 * x))
-
-
-def f12(x):
-    return sin(x) ** 3 + cos(x) ** 3, 3 * sin(x) * cos(x) * (sin(x) - cos(x))
-
-
-def f18(x):
-    if x <= 3:
-        return (x - 2) ** 2, 2 * (x - 2)
-    else:
-        return 2 * log(x - 2) + 1, 2 / (x - 2)
-
-
-def _func_wrapper(func: Callable[[float], Tuple[float, float]]) -> Callable[[float], Tuple[float, float]]:
+def _func_wrapper(
+    func: Callable[[float], Tuple[float, float]]
+) -> Callable[[float], Tuple[float, float]]:
     def inner(x: float) -> Tuple[float, float]:
         inner.calls += 1
         return func(x)
@@ -67,8 +55,15 @@ def _func_wrapper(func: Callable[[float], Tuple[float, float]]) -> Callable[[flo
     return inner
 
 
-funcs: List[Tuple[int, Callable[[float], Tuple[float, float]], Tuple[float, float], Optional[float]]] = [
-    (0, _func_wrapper(f0), (-1000, 239), 0.0),
+funcs: List[
+    Tuple[
+        int,
+        Callable[[float], Tuple[float, float]],
+        Tuple[float, float],
+        Optional[float],
+    ]
+] = [
+    (0, _func_wrapper(f0), (-5, 5), 0.0),
     # 02
     (2, _func_wrapper(f2), (2.7, 7.5), None),
     (2, _func_wrapper(f2), (4.5, 6.0), 5.145735),
@@ -96,22 +91,6 @@ funcs: List[Tuple[int, Callable[[float], Tuple[float, float]], Tuple[float, floa
     # 12
     (12, _func_wrapper(f12), (0, 2 * pi), None),
     (12, _func_wrapper(f12), (1.7, 6), pi),
-    # 13
-    # (13, _func_wrapper(lambda x: -(x ** (2 / 3)) - (1 - x ** 2) ** (1 / 3)), (0.001, 0.99), 1 / 2 ** 0.5),
-    # 14
-    # (14, _func_wrapper(lambda x: -exp(-x) * sin(2 * pi * x)), (0, 4), None),
-    # (14, _func_wrapper(lambda x: -exp(-x) * sin(2 * pi * x)), (0, 0.6), 0.224885),
-    # 15
-    # (15, _func_wrapper(lambda x: (x ** 2 - 5 * x + 6) / (x ** 2 + 1)), (-5, 5), None),
-    # (15, _func_wrapper(lambda x: (x ** 2 - 5 * x + 6) / (x ** 2 + 1)), (-0.1, 5), 2.41422),
     # 18
     (18, _func_wrapper(f18), (0, 6), 2),
-    # 20
-    # (20, _func_wrapper(lambda x: -(x - sin(x)) * exp(-(x ** 2))), (-10, 10), 1.195137),
-    # 21
-    # (21, _func_wrapper(lambda x: x * sin(x) + x * cos(2 * x)), (0, 10), None),
-    # (21, _func_wrapper(lambda x: x * sin(x) + x * cos(2 * x)), (3, 6.5), 4.79507),
-    # 22
-    # (22, _func_wrapper(lambda x: exp(-3 * x) - sin(x) ** 3), (0, 20), None),
-    # (22, _func_wrapper(lambda x: exp(-3 * x) - sin(x) ** 3), (12, 16), 9 * pi / 2),
 ]
--- solver.py
+++ solver.py
@@ -9,12 +9,15 @@ def optimize(f, a: float, b: float, eps: float = 1e-8):
     return brent_d(f, a, b, eps)[0]
 
 
-def brent_d(func: Callable[[float], Tuple[float, float]], a: float, b: float, eps: float = 1e-8):
-    max_iter = 50
+def brent_d(
+    func: Callable[[float], Tuple[float, float]], a: float, b: float, eps: float = 1e-8
+):
+    max_iter = 500
     history = []
     zeps = eps * 1e-1
 
-    x = w = v = (a + b) / 2  # x is a current minimum, w is a second minimum, v is a previous w
+    # x is a current minimum, w is a second minimum, v is a previous w
+    x = w = v = (a + b) / 2
     f, df = func(x)
     fx = fw = fv = f
     dx = dw = dv = df
@@ -31,15 +34,10 @@ def brent_d(func: Callable[[float], Tuple[float, float]], a: float, b: float, ep
             d1, d2 = 2.0 * (b - a), 2.0 * (b - a)
 
             # Fitting parabolas
-            ds = []
             if dw != dx:
                 d1 = (w - x) * dx / (dx - dw)
-                # d3 = cubic_minima(w, x, fw, fx, dw, dx)
-                # print(f"par: {d1}, cubic: {d3}")
             if dv != dx:
                 d2 = (v - x) * dx / (dx - dv)
-                # d4 = cubic_minima(v, x, fv, fx, dv, dx)
-                # print(f"par: {d2}, cubic: {d4}")
 
             u1, u2 = x + d1, x + d2
 
@@ -70,11 +68,12 @@ def brent_d(func: Callable[[float], Tuple[float, float]], a: float, b: float, ep
             u = x + diff
             fu, du = func(u)
         else:
-            u = x + copysign(tol1, diff)
+            u = x + copysign(tol1, diff)  # adjusting at least by tol1
             fu, du = func(u)
             if fu > fx:
                 return x, history
 
+        # Updating the state
         if fu <= fx:
             if u >= x:
                 a = x
@@ -97,34 +96,6 @@ def brent_d(func: Callable[[float], Tuple[float, float]], a: float, b: float, ep
     return x, history
 
 
-def parabola_minima_d(x_1: float, x_2: float, d_1: float, d_2: float) -> Optional[float]:
-    if not isclose(x_1, x_2) and not isclose(d_1, d_2):
-        d = d_1 * (x_2 - x_1) / (d_1 - d_2)
-        u = x_1 + d
-        if not isclose(x_1, u) and not isclose(x_2, u) and d * d_1 <= 0.0:
-            return u
-
-
-def cubic_minima(x1: float, x2: float, f1: float, f2: float, d1: float, d2: float) -> Optional[float]:
-    h = x2 - x1
-    F = f2 - f1
-    G = (d2 - d1) * h
-    c = G - 2 * (F - d1 * h)
-    under_root = (G - 3 * c) ** 2 - 12 * c * d1 * h
-    if under_root >= 0:
-        gamma = -2 * d1 * h / ((G - 3 * c) + sqrt(under_root) + 1e-8)
-        return h * gamma - h
-    else:
-        b = d2 * h - F
-        gamma = (1 - F / b) / 2
-        return h * gamma - h
-
-
-def choose_best_u(x: float, a: float, b: float, eps: float, us: List[Optional[float]]) -> Optional[float]:
-    us = [u for u in us if u is not None and a + eps < u < b - eps]
-    return min(us, key=lambda u: abs(x - u)) if us else None
-
-
 def main():
     eps = 1e-8
     total_score_1 = 0
@@ -133,16 +104,24 @@ def main():
     for no, func, (a, b), x_target in funcs:
         x_predicted, history = brent_d(func, a, b, eps)
         if x_target is not None:
-            if abs(x_target - x_predicted) < 10 ** max(decimal.Decimal(str(x_target)).as_tuple().exponent, -8):
+            if abs(x_target - x_predicted) < 10 ** max(
+                decimal.Decimal(str(x_target)).as_tuple().exponent, -8
+            ):
                 print(f"#{no} -- Successfully found x with {func.calls} oracul calls")
             else:
-                print(f"#{no} -- FAILED: x_target: {x_target}    |     x_predicted: {x_predicted}")
+                print(
+                    f"#{no} -- FAILED: x_target: {x_target}    |     x_predicted: {x_predicted}"
+                )
                 fails_number += 1
             total_score_1 += func.calls
         else:
-            print(f"#{no} -- Found x: {x_predicted} with {func.calls} oracul calls, y is {func(x_predicted)[0]}")
+            print(
+                f"#{no} -- Found x: {x_predicted} with {func.calls} oracul calls, y is {func(x_predicted)[0]}"
+            )
             total_score_2 += func.calls
-    print(f"TOTAL SCORE FOR UNI IS {total_score_1} FOR NON-UNI IS {total_score_2} WITH {fails_number} FAILS")
+    print(
+        f"TOTAL SCORE FOR UNI IS {total_score_1} FOR NON-UNI IS {total_score_2} WITH {fails_number} FAILS"
+    )
 
 
 if __name__ == "__main__":
